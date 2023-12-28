c8faa25ae1eb742d7377f6315ff0b7fd6311c9dd
hüseyin gülerman <huseyingulerman.1997@gmail.com>
2023-12-28 13:31:06 +0300

    .

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Fimple_Cohorts_Model_Kullanimi.BookOperations;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using static Fimple_Cohorts_Model_Kullanimi.BookOperations.GetByIdQuery;
+using static Fimple_Cohorts_Model_Kullanimi.BookOperations.UpdateBookCommand;
 
 namespace Fimple_Cohorts_Model_Kullanimi.Controllers
 {
@@ -7,5 +10,44 @@ namespace Fimple_Cohorts_Model_Kullanimi.Controllers
     [ApiController]
     public class BookController : ControllerBase
     {
+        private readonly BookStoreDbContext _dbContext;
+        public BookController(BookStoreDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetByID(int id)
+        {
+            GetByIdQuery query = new GetByIdQuery(_dbContext);
+            BooksViewModel result = new BooksViewModel();
+            try
+            {
+                result = query.Handle(id);
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex.Message);
+            }
+
+
+            return Ok(result);
+        }
+        [HttpPut("{id}")]
+        public IActionResult UpdateBook(int id, [FromBody] UpdateBookModel updatedBook)
+        {
+            UpdateBookCommand command = new UpdateBookCommand(_dbContext);
+            try
+            {
+                command.Model = updatedBook;
+                command.Handle(id);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            return RedirectToAction("GetByID", new { id = id });
+        }
     }
 }
